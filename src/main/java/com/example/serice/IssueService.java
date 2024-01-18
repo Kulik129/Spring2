9120a1c41039461e1ed43516bc837937b08ef107
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2024-01-19 00:10:59 +0300

    Бизнес логика

---
+++ src/main/java/com/example/serice/IssueService.java
@@ -0,0 +1,32 @@
+package com.example.serice;
+
+import com.example.api.IssueRequest;
+import com.example.model.Issue;
+import com.example.repository.BookRepository;
+import com.example.repository.IssueRepository;
+import com.example.repository.ReaderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class IssueService {
+    private final BookRepository bookRepository;
+    private final ReaderRepository readerRepository;
+    private final IssueRepository issueRepository;
+
+    public Issue issue(IssueRequest request) {
+        if (bookRepository.getBookById(request.getBookId()) == null) {
+            throw new NoSuchElementException("Не найдена книга с ID \"" + request.getBookId() + "\"");
+        }
+        if (readerRepository.getReaderById(request.getReaderId()) == null) {
+            throw new NoSuchElementException("Не найден читатель с ID \"" + request.getReaderId() + "\"");
+        }
+
+        Issue issue = new Issue(request.getBookId(), request.getReaderId());
+        issueRepository.save(issue);
+        return issue;
+    }
+}
