f2aa53b63d1eb3ca1257387f27aad86d01163d05
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2024-01-19 00:10:09 +0300

    Модели

---
+++ src/main/java/com/example/model/Issue.java
@@ -0,0 +1,24 @@
+package com.example.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Issue {
+    private static long sequence = 1L;
+
+
+    private final long id;
+    private final long bookId;
+    private final long readerId;
+    private final LocalDateTime timestamp;
+
+    public Issue(long bookId, long readerId) {
+        this.id = sequence++;
+        this.bookId = bookId;
+        this.readerId = readerId;
+        this.timestamp = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/example/model/Reader.java
@@ -0,0 +1,16 @@
+package com.example.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Reader {
+    private static long sequence = 1L;
+    private final long id;
+    private final String name;
+
+    public Reader(String name) {
+        this(sequence++, name);
+    }
+}
