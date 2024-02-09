b9e8a15d44e645b42758092638899a83cdb42866
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2024-02-09 16:46:30 +0300

    Убрал security для упрощения взаимодествия

--- src/main/java/com/example/security/CustomService.java
+++ src/main/java/com/example/security/CustomService.java
@@ -1,27 +1,27 @@
-package com.example.security;
-
-import com.example.model.User;
-import com.example.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-@Component
-@RequiredArgsConstructor
-public class CustomService implements UserDetailsService {
-    private final UserRepository userRepository;
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByLogin(username)
-                .orElseThrow(() -> new UsernameNotFoundException(username));
-
-        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), List.of(
-                new SimpleGrantedAuthority(user.getRole())
-        ));
-    }
-}
+//package com.example.security;
+//
+//import com.example.model.User;
+//import com.example.repository.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class CustomService implements UserDetailsService {
+//    private final UserRepository userRepository;
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User user = userRepository.findByLogin(username)
+//                .orElseThrow(() -> new UsernameNotFoundException(username));
+//
+//        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), List.of(
+//                new SimpleGrantedAuthority(user.getRole())
+//        ));
+//    }
+//}
